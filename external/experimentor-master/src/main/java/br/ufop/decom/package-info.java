e90741e0bf24292e810d51ee56d5dd1322530951
Lucas Natali <lucasnatali16@gmail.com>
2022-10-06 20:23:06 -0300

    add task executor project

--- external/experimentor-master/src/main/java/br/ufop/decom/package-info.java
+++
@@ -1,9 +0,0 @@
-@XmlSchema(
-        namespace = "http://www.decom.ufop.br",
-        elementFormDefault = XmlNsForm.QUALIFIED
-)
-
-package br.ufop.decom;
-
-import javax.xml.bind.annotation.XmlNsForm;
-import javax.xml.bind.annotation.XmlSchema;
