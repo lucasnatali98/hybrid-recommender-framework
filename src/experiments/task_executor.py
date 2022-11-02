0ae99e31e80ccb9d274db76fc0eff2b5b4d52d1a
Lucas Natali <lucas.natali@upflow.me>
2022-11-02 10:29:16 -0300

    add task executor class to handle with task executor project

---
+++ src/experiments/task_executor.py
@@ -0,0 +1,18 @@
+
+class TaskExecutor:
+    def __init__(self):
+        pass
+
+    def build_task_executor(self):
+        """
+
+        @return:
+        """
+        pass
+
+    def deploy_task_executor(self):
+        """
+
+        @return:
+        """
+        pass
