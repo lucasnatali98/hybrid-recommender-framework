e977ac32e011b94e9a3d5f9da2d01ab53d96bc3e
Lucas Natali <lucas.natali@upflow.me>
2022-11-02 10:39:57 -0300

    add subprocess tasks to task executor deploy

--- src/experiments/task_executor.py
+++ src/experiments/task_executor.py
@@ -1,4 +1,4 @@
-
+import subprocess
 class TaskExecutor:
     def __init__(self):
         pass
@@ -8,11 +8,14 @@ class TaskExecutor:
 
         @return:
         """
-        pass
+
+        output = subprocess.run(['sh', 'external/task-executor/build_image.sh'], capture_output=True)
+        return output
 
     def deploy_task_executor(self):
         """
 
         @return:
         """
-        pass
+        output = subprocess.run(['sh', 'external/task-executor/deploy.sh'], capture_output=True)
+        return output
