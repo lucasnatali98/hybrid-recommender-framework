09cb2093f6da61247452ad70e35c7688c3dfdc67
Lucas <lucasnatali16@gmail.com>
2022-11-03 21:31:10 -0300

    Reorganizando classes de experimentação

--- src/experiments/experiment.py
+++ src/experiments/experiment.py
@@ -1,9 +1,7 @@
 from abc import ABC, abstractmethod
-from src.instance_factory import InstanceFactory
 
 
-class Experiment(ABC):
-
+class AbstractExperiment(ABC):
 
     @abstractmethod
     def run(self):
@@ -22,35 +20,28 @@ class Experiment(ABC):
         """
         pass
 
-    @abstractmethod
-    def add(self, experiment):
+
+class Experiment:
+    """
+
         """
 
-        @param experiment:
-        @return:
-        """
-        pass
+    experiment_id: str
+    datasets: object
+    preprocessing: object
+    metrics: object
+    metafeatures: object
+    results: object
+    visualization: object
+    recommenders: object
 
-    @abstractmethod
-    def insert(self, experiments):
-        """
-
-        @param experiments:
-        @return:
-        """
-        pass
-
-    @abstractmethod
-    def remove(self, experiment):
+    def __init__(self) -> None:
         """
 
-        @param experiment:
-        @return:
         """
-        pass
+        self.experiments = []
 
-    @abstractmethod
-    def removeAll(self):
+    def run(self):
         """
 
         @return:
@@ -58,3 +49,57 @@ class Experiment(ABC):
         pass
 
 
+    def __str__(self):
+        print(
+            "E"
+        )
+
+    def _handle_with_dataset(self, dataset):
+        pass
+
+    def _set_attributes(self, instances: dict):
+        self.datasets = instances['datasets']
+        self.metafeatures = instances['metafeatures']
+        self.preprocessing = instances['preprocessing']
+        self.results = instances['results']
+        self.visualization = instances['visualization']
+        self.recommenders = instances['recommenders']
+        self.metrics = instances['metrics']
+
+    def create_experiment_instances(self, config_obj) -> dict:
+        """
+
+
+
+        @param config_obj:
+        @return:
+        """
+        instance_factory = InstanceFactory(config_obj[0])
+
+        dataset_dict = instance_factory.get_instance_from_config_obj("MovieLens")
+
+        pre_processing_instance_dict = instance_factory.get_instance_from_config_obj("PreProcessingContainer")
+
+        metrics_instance_dict = instance_factory.get_instance_from_config_obj("MetricsContainer")
+        metafeatures_instance_dict = instance_factory.get_instance_from_config_obj("MetaFeatureContainer")
+        recommenders_instance_dict = instance_factory.get_instance_from_config_obj("RecommendersContainer")
+        visualization_instance_dict = instance_factory.get_instance_from_config_obj("VisualizationContainer")
+        results_instance_dict = instance_factory.get_instance_from_config_obj("ResultsContainer")
+
+        dataset_instance = instance_factory.create_instance(dataset_dict)
+        preprocessing_instance = instance_factory.create_instance(pre_processing_instance_dict)
+        metrics_instance = instance_factory.create_instance(metrics_instance_dict)
+        metafeatures_instance = instance_factory.create_instance(metafeatures_instance_dict)
+        recommenders_instance = instance_factory.create_instance(recommenders_instance_dict)
+        visualization_instance = instance_factory.create_instance(visualization_instance_dict)
+        results_instance = instance_factory.create_instance(results_instance_dict)
+
+        return {
+            "datasets": dataset_instance,
+            "preprocessing": preprocessing_instance,
+            "metrics": metrics_instance,
+            "metafeatures": metafeatures_instance,
+            "visualization": visualization_instance,
+            "recommenders": recommenders_instance,
+            "results": results_instance
+        }
--- src/experiments/experiment_handler.py
+++ src/experiments/experiment_handler.py
@@ -1,20 +1,10 @@
 from dataclasses import dataclass
 from src.experiments.experiment import Experiment
+from src.instance_factory import InstanceFactory
+from src.experiments.experiment import AbstractExperiment
 
-class ExperimentHandler(Experiment):
-    """
-
-    """
-
-    experiment_id: str
-    datasets: object
-    preprocessing: object
-    metrics: object
-    metafeatures: object
-    results: object
-    visualization: object
-    recommenders: object
-
+class ExperimentHandler:
+    experiment: AbstractExperiment
     def __init__(self) -> None:
         """
 
@@ -28,67 +18,10 @@ class ExperimentHandler(Experiment):
         """
         pass
 
-    def _handle_with_dataset(self, dataset):
+   def _handle_with_dataset(self, dataset):
         pass
 
-    def _set_attributes(self, instances: dict):
-        self.datasets = instances['datasets']
-        self.metafeatures = instances['metafeatures']
-        self.preprocessing = instances['preprocessing']
-        self.results = instances['results']
-        self.visualization = instances['visualization']
-        self.recommenders = instances['recommenders']
-        self.metrics = instances['metrics']
 
-    def create_experiment_instances(self, config_obj) -> dict:
-        """
-
-
-
-        @param config_obj:
-        @return:
-        """
-        instance_factory = InstanceFactory(config_obj[0])
-
-        dataset_dict = instance_factory.get_instance_from_config_obj("MovieLens")
-
-        pre_processing_instance_dict = instance_factory.get_instance_from_config_obj("PreProcessingContainer")
-
-        metrics_instance_dict = instance_factory.get_instance_from_config_obj("MetricsContainer")
-        metafeatures_instance_dict = instance_factory.get_instance_from_config_obj("MetaFeatureContainer")
-        recommenders_instance_dict = instance_factory.get_instance_from_config_obj("RecommendersContainer")
-        visualization_instance_dict = instance_factory.get_instance_from_config_obj("VisualizationContainer")
-        results_instance_dict = instance_factory.get_instance_from_config_obj("ResultsContainer")
-
-        dataset_instance = instance_factory.create_instance(dataset_dict)
-        preprocessing_instance = instance_factory.create_instance(pre_processing_instance_dict)
-        metrics_instance = instance_factory.create_instance(metrics_instance_dict)
-        metafeatures_instance = instance_factory.create_instance(metafeatures_instance_dict)
-        recommenders_instance = instance_factory.create_instance(recommenders_instance_dict)
-        visualization_instance = instance_factory.create_instance(visualization_instance_dict)
-        results_instance = instance_factory.create_instance(results_instance_dict)
-
-        return {
-            "datasets": dataset_instance,
-            "preprocessing": preprocessing_instance,
-            "metrics": metrics_instance,
-            "metafeatures": metafeatures_instance,
-            "visualization": visualization_instance,
-            "recommenders": recommenders_instance,
-            "results": results_instance
-        }
-
-    def set_experiment(self, experiment: dict) -> None:
-        """
-
-        @param experiments:
-        @return:
-        """
-
-        if bool(experiment) == False:
-            print("Experiment object is empty")
-
-        self.experiments.append(experiment)
 
     def add(self, experiment) -> None:
         """
