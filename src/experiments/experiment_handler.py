fc4b0ecefc7da691278ce1780d4a2359f07942bb
Lucas <lucasnatali16@gmail.com>
2022-11-03 21:46:46 -0300

    Experiment handler é um Container

--- src/experiments/experiment_handler.py
+++ src/experiments/experiment_handler.py
@@ -1,52 +1,10 @@
 from src.experiments.experiment import AbstractExperiment
+from src.shared.container import Container
+class ExperimentHandler(Container):
 
-class ExperimentHandler:
-    experiment: AbstractExperiment
-    def __init__(self) -> None:
+    def __init__(self, experiments) -> None:
         """
 
         """
-        self.experiments = []
-
-    def run(self):
-        """
-
-        @return:
-        """
-        pass
-
-   def _handle_with_dataset(self, dataset):
-        pass
-
-
-
-    def add(self, experiment) -> None:
-        """
-
-        @param experiment:
-        @return:
-        """
-        pass
-
-    def insert(self, experiments) -> None:
-        """
-
-        @param experiments:
-        @return:
-        """
-        pass
-
-    def remove(self, experiment) -> None:
-        """
-
-        @param experiment: dict
-        @return:
-        """
-        self.experiments.remove(experiment)
-
-    def removeAll(self) -> None:
-        """
-
-        @return:
-        """
-        self.experiments.clear()
+        super().__init__()
+        self.insert(0, experiments)
