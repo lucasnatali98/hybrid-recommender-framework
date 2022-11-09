a02d794c925b814c40e5f9728b8d16fe54edf9f4
Lucas <lucasnatali16@gmail.com>
2022-11-09 07:37:54 -0300

    Atualizando portas no arquivos de deployment kubernetes

--- deploy_task_executor_homolog.sh
+++ deploy_task_executor_homolog.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+gcloud container clusters get-credentials autopilot-cluster-1 --zone us-central1 --project thematic-mapper-364320 && \
+
+cat <<EOF > task-executor-deployment.yaml
 ---
 apiVersion: "apps/v1"
 kind: "Deployment"
@@ -21,3 +26,9 @@ spec:
         image: "lucasnatali98/task-executor:task-executor-homolog-1.0.0"
         ports:
         - containerPort: 5050
+EOF
+
+kubectl apply -f task-executor-deployment.yaml && \
+kubectl get pods
+kubectl expose deployment task-executor-deployment --type=LoadBalancer --name=task-executor-service-homolog
+kubectl describe service task-executor-service-homolog | grep IP
--- deploy_xperimentor_homolog.sh
+++ deploy_xperimentor_homolog.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+gcloud container clusters get-credentials autopilot-cluster-1 --zone us-central1 --project thematic-mapper-364320 && \
+
+cat <<EOF > xperimentor-deployment.yaml
 ---
 apiVersion: "apps/v1"
 kind: "Deployment"
@@ -21,3 +26,9 @@ spec:
         image: "lucasnatali98/xperimentor:xperimentor-homolog-1.0.1"
         ports:
         - containerPort: 3000
+EOF
+
+kubectl apply -f xperimentor-deployment.yaml && \
+kubectl get pods
+kubectl expose deployment xperimentor-deployment --type=LoadBalancer --name=xperimentor-service-homolog
+kubectl describe service xperimentor-service-homolog | grep IP
