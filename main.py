7f5a207f1f791fc467bb94ff57d5226d573dc5cc
Lucas <lucasnatali16@gmail.com>
2022-12-24 10:48:33 -0300

    ...

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import subprocess
+
 from src.data.loader import Loader
 from external.deploy import TaskExecutor, Xperimentor
 from src.parser import json2yaml
@@ -7,51 +9,53 @@ from src.experiments.experiment_tasks import ExperimentTask
 from external.deploy import Xperimentor
 
 if __name__ == "__main__":
-    loader = Loader()
-    config_obj = loader.load_json_file("config.json")
+        loader = Loader()
+        config_obj = loader.load_json_file("config.json")
 
-    experiments = config_obj['experiments']
+        experiments = config_obj['experiments']
 
-    cluster_info = config_obj['cluster_info']
+        cluster_info = config_obj['cluster_info']
 
-    recipes_default = config_obj['recipesDefault']
+        recipes_default = config_obj['recipesDefault']
 
-    experiment_dependencies = config_obj['experiment_dependencies']
+        experiment_dependencies = config_obj['experiment_dependencies']
 
-    experiment_handler = ExperimentHandler(
-        experiments=experiments
-    )
-    experiment_task = ExperimentTask()
-    experiment_tasks = experiment_task.define_all_tasks()
-    print("Todas as tarefas do experimento")
-    print(experiment_tasks)
+        experiment_handler = ExperimentHandler(
+            experiments=experiments
+        )
+        experiment_task = ExperimentTask()
+        experiment_tasks = experiment_task.define_all_tasks()
+        print("Todas as tarefas do experimento")
+        print(experiment_tasks)
 
-    xperimentor = Xperimentor()
-    xperimentor_config_obj = xperimentor.convert_to_xperimentor_pattern(
-        experiments=experiments,
-        experiment_dependencies=experiment_dependencies,
-        recipes_default=recipes_default,
-        cluster_info=cluster_info,
-        tasks=experiment_tasks
-    )
+        xperimentor = Xperimentor()
+        xperimentor_config_obj = xperimentor.convert_to_xperimentor_pattern(
+            experiments=experiments,
+            experiment_dependencies=experiment_dependencies,
+            recipes_default=recipes_default,
+            cluster_info=cluster_info,
+            tasks=experiment_tasks
+        )
 
-    with open("experiment_output/configuration_files/xperimentor_yaml_file.yaml", 'w') as file:
-        xperimentor_yaml_file = json2yaml(xperimentor_config_obj, file)
-
-
-   # experiment_results = experiment_handler.run_experiments()
-
-    path_to_config_file = "";
-
-    args = sys.argv
-
-    if len(args) == 0:
-        path_to_config_file = "config.json"
-    else:
-        path_to_config_file = args[1]
+        with open("experiment_output/configuration_files/xperimentor_yaml_file.yaml", 'w') as file:
+            xperimentor_yaml_file = json2yaml(xperimentor_config_obj, file)
 
 
-   # dataset_task = run_dataset_task()
-    print("Dataset task result")
+       # experiment_results = experiment_handler.run_experiments()
+
+        path_to_config_file = "";
+
+        args = sys.argv
+
+        if len(args) == 0:
+            path_to_config_file = "config.json"
+        else:
+            path_to_config_file = args[1]
 
 
+       # dataset_task = run_dataset_task()
+        print("Dataset task result")
+        dataset_task_command = experiment_tasks[0]['command']
+        print("dataset task command: ", dataset_task_command)
+        output = subprocess.run([dataset_task_command], shell=True)
+        print("Output do processo: ", output)
