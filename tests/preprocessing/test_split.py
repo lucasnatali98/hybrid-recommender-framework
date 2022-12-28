853be9d0834f70733fe674e76b975f2139fefcbf
Lucas <lucasnatali16@gmail.com>
2022-12-28 17:02:40 -0300

    Adicionando classes para testar o pre processamento

---
+++ tests/preprocessing/test_split.py
@@ -0,0 +1,6 @@
+import pytest
+
+class TestSplitProcessing:
+
+    def test_pre_processing(self):
+        pass
---
+++ tests/preprocessing/test_text.py
@@ -0,0 +1,6 @@
+import pytest
+
+class TestTextProcessing:
+
+    def test_pre_processing(self):
+        pass
