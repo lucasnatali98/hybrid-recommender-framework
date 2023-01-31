7ddde07b749b8946cc156f7c50f78bd42327b6b2
Lucas <lucasnatali16@gmail.com>
2023-01-31 20:50:15 -0300

    Adicionando implementações iniciais para o intervalo de confiança

--- src/metrics/mae.py
+++ src/metrics/mae.py
@@ -1,12 +1,15 @@
 from src.metrics.metric import PredictionMetric
 import lenskit.metrics.predict as lenskit_predict
 import pandas as pd
+from sklearn.metrics import mean_absolute_error
 from src.utils import process_parameters
+
 
 class MAE(PredictionMetric):
     """
 
     """
+
     def __init__(self, parameters: dict) -> None:
         """
         
@@ -15,12 +18,24 @@ class MAE(PredictionMetric):
         parameters = process_parameters(parameters, default_keys)
 
     def evaluate(self, predictions: pd.Series, truth: pd.Series, **kwargs):
-        """
+        raise NotImplementedError
 
-        @param predictions:
-        @param truth:
-        @return:
-        """
+
+class LenskitMAE(MAE):
+    def __init__(self, parameters: dict) -> None:
+        super().__init__(parameters)
+        default_keys = set()
+        parameters = process_parameters(parameters, default_keys)
+
+    def evaluate(self, predictions: pd.Series, truth: pd.Series, **kwargs):
         return lenskit_predict.mae(predictions, truth)
 
 
+class ScikitMAE(MAE):
+    def __init__(self, parameters: dict) -> None:
+        super().__init__(parameters)
+        default_keys = set()
+        parameters = process_parameters(parameters, default_keys)
+
+    def evaluate(self, predictions: pd.Series, truth: pd.Series, **kwargs):
+        return mean_absolute_error(truth, predictions, **kwargs)
--- src/results/confidence_interval.py
+++ src/results/confidence_interval.py
@@ -1,23 +1,23 @@
 from src.results.results import AbstractResults
-from scipy.stats import t, norm
+from scipy.stats import t, norm, ttest_1samp, ttest_ind, ttest_ind_from_stats, ttest_rel
 from src.utils import process_parameters
+from scipy import stats
+import numpy as np
 
 
 class ConfidenceInterval(AbstractResults):
 
     def __init__(self, parameters: dict) -> None:
-        """
-        
-        """
         default_keys = set()
         parameters = process_parameters(parameters, default_keys)
+
         self.loc = parameters.get('loc')
         self.sigma = parameters.get('sigma')
         self.scale = parameters.get('scale')
         self.type = parameters.get('type')
         self.alpha = parameters.get('alpha')
 
-    def get_results(self):
+    def get_results(self, sample_data, **kwargs):
         """
 
         @return:
@@ -29,3 +29,53 @@ class ConfidenceInterval(AbstractResults):
             return norm.interval()
 
         return None
+
+
+class ConfidenceIntervalMean(ConfidenceInterval):
+    def __init__(self, parameters: dict) -> None:
+        super().__init__(parameters)
+        default_keys = set()
+        parameters = process_parameters(parameters, default_keys)
+
+    def get_results(self, sample_data, **kwargs):
+        return stats.t.interval(
+            alpha=0.95,
+            loc=np.mean(sample_data),
+            df=len(sample_data) - 1,
+            scale=stats.sem(sample_data)
+        )
+
+
+class ConfidenceIntervalBinomial(ConfidenceInterval):
+    def __init__(self, parameters: dict) -> None:
+        super().__init__(parameters)
+        default_keys = set()
+        parameters = process_parameters(parameters, default_keys)
+
+    def get_results(self, sample_data, **kwargs):
+        return stats.binom.interval(
+            alpha=0.99,
+            n=len(sample_data) - 1,
+            loc=np.mean(sample_data),
+            p=stats.sem(sample_data)
+        )
+
+
+class ConfidenceIntervalLinearDifference(ConfidenceInterval):
+    def __init__(self, parameters: dict) -> None:
+        super().__init__(parameters)
+        default_keys = set()
+        parameters = process_parameters(parameters, default_keys)
+
+    def get_results(self, sample_data, **kwargs):
+        sample_data = np.array(sample_data)
+        sample_data2 = np.array(kwargs.get('sample_data2'))
+
+        diffsamp = sample_data - sample_data2
+        len_no_obs = len(sample_data)
+        diffmean = np.mean(diffsamp)
+        diffvar = np.var(diffsamp, ddof=1)
+        criticalvalue = stats.t.ppf(q=1 - alp / 2, df=len_no_obs - 1)
+        rad = criticalvalue * np.sqrt(diffvar) / np.sqrt(len_no_obs)
+
+        return [diffmean - rad, diffmean + rad]
