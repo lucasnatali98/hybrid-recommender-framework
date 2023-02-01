7befd1f54ca8207177f1e42b5878e99818dc0adf
Lucas <lucasnatali16@gmail.com>
2023-01-31 21:48:43 -0300

    Incrementando funções dos resultados de intervalo de confiança

--- src/results/confidence_interval.py
+++ src/results/confidence_interval.py
@@ -18,17 +18,7 @@ class ConfidenceInterval(AbstractResults):
         self.alpha = parameters.get('alpha')
 
     def get_results(self, sample_data, **kwargs):
-        """
-
-        @return:
-        """
-        if self.type == "norm":
-            return norm.interval()
-
-        if self.type == "t":
-            return norm.interval()
-
-        return None
+        raise NotImplementedError
 
 
 class ConfidenceIntervalMean(ConfidenceInterval):
@@ -36,10 +26,11 @@ class ConfidenceIntervalMean(ConfidenceInterval):
         super().__init__(parameters)
         default_keys = set()
         parameters = process_parameters(parameters, default_keys)
+        self.alpha = parameters.get('alpha', 0.95)
 
     def get_results(self, sample_data, **kwargs):
         return stats.t.interval(
-            alpha=0.95,
+            alpha=self.alpha,
             loc=np.mean(sample_data),
             df=len(sample_data) - 1,
             scale=stats.sem(sample_data)
@@ -51,10 +42,10 @@ class ConfidenceIntervalBinomial(ConfidenceInterval):
         super().__init__(parameters)
         default_keys = set()
         parameters = process_parameters(parameters, default_keys)
-
+        self.alpha = parameters.get('alpha', 0.95)
     def get_results(self, sample_data, **kwargs):
         return stats.binom.interval(
-            alpha=0.99,
+            alpha=self.alpha,
             n=len(sample_data) - 1,
             loc=np.mean(sample_data),
             p=stats.sem(sample_data)
@@ -75,7 +66,9 @@ class ConfidenceIntervalLinearDifference(ConfidenceInterval):
         len_no_obs = len(sample_data)
         diffmean = np.mean(diffsamp)
         diffvar = np.var(diffsamp, ddof=1)
-        criticalvalue = stats.t.ppf(q=1 - alp / 2, df=len_no_obs - 1)
+
+        #O valor 0.3 está chumbado e precisa ser alterado para sua variável correta
+        criticalvalue = stats.t.ppf(q=1 - 0.3 / 2, df=len_no_obs - 1)
         rad = criticalvalue * np.sqrt(diffvar) / np.sqrt(len_no_obs)
 
         return [diffmean - rad, diffmean + rad]
